483929d92488e7a01579479437e72d64d531203b
bruno-prata <email.prata@gmail.com>
2020-05-09 10:15:13 -0300

    Form Ready

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-const Controller = require('../controllers/Controller');
+const Controller = require('../controller/Controller');
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
